23032a996d9eb2895b6a4329ac22fdf289a4ab12
sujiayi317 <sujiayi317@gmail.com>
2020-05-20 16:16:57 -0400

    Add an Up button in the app bar

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -19,6 +19,8 @@ package com.example.android.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -26,6 +28,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        // use the navigation controller to add an Up button:
+        // 1.find the navigation controller object:
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        // 2.link the navigation controller to the app bar:
+        NavigationUI.setupActionBarWithNavController(this,navController)
+    }
+    // 3.override the onSupportNavigateUp() method to call navigateUp() in the navigation controller:
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 
     // TODO (01) Create the new TitleFragment
