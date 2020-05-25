7f909eaca6eecc7eeffa8d7eb7039b9134db2140
sujiayi317 <sujiayi317@gmail.com>
2020-05-25 17:49:24 -0400

    Set up and use the Safe Args plugin; Add and pass arguments

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -20,6 +20,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -36,6 +37,12 @@ class GameWonFragment : Fragment() {
         binding.nextMatchButton.setOnClickListener{view: View->
             view.findNavController()
                     .navigate(R.id.action_gameWonFragment_to_gameFragment)}
+
+        // extract the arguments from the bundle, then use a Toast to display the arguments:
+        val args = GameWonFragmentArgs.fromBundle(arguments!!)
+        Toast.makeText(context,
+                "NumCorrect: ${args.numCorrect}, " + "NumQuestions: ${args.numQuestions}",
+                Toast.LENGTH_LONG).show()
         return binding.root
     }
 }
